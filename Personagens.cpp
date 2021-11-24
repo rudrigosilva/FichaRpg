1a18298811f5e7063e6e2762fc8e159283025e2b
Rodrigo Silva <rodrigosilvasouza616@gmail.com>
2021-11-24 08:01:11 -0300

    classe personagens

---
+++ Personagens.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include"Personagens.h"
+
+using namespace std;
+
+Personagens::Personagens(){
+    this->nome = "";
+    this->classe = "";
+    this->nivel = 0;
+    this->alinhamento = "";
+}
--- Personagens.h
+++ Personagens.h
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <String>
 
 using namespace std;
 
